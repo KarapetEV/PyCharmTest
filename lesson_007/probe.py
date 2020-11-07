89a350b84b4d8e20bb2fba0249592d43cf6f3e28
Evgeniy Karapyshev <karapet2011@gmail.com>
2020-11-07 16:44:47 +0300

    Первая версия на 20 дней жизни

---
+++ lesson_007/probe.py
@@ -0,0 +1,100 @@
+from random import randint
+
+# Реализуем модель человека.
+# Человек может есть, работать, играть, ходить в магазин.
+# У человека есть степень сытости, немного еды и денег.
+# Если сытость < 0 единиц, человек умирает.
+# Человеку надо прожить 365 дней.
+from termcolor import cprint
+
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 50
+        self.fullness -= 10
+
+    def watch_MTV(self):
+        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint('{} деньги закончились!'.format(self.name), color='red')
+
+    def go_into_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} въехал в дом!!!'.format(self.name), color='cyan')
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watch_MTV()
+
+
+class House:
+
+    def __init__(self):
+        self.food = 10
+        self.money = 50
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}'.format(self.food, self.money)
+
+
+beavis = Man(name='Бивис')
+butthead = Man(name='Батхед')
+
+my_sweet_home = House()
+
+beavis.go_into_the_house(house=my_sweet_home)
+butthead.go_into_the_house(house=my_sweet_home)
+
+for day in range(1, 21):
+    print('===================== день {} ====================='.format(day))
+    beavis.act()
+    butthead.act()
+    print('------------------ в конце дня -------------------')
+    print(beavis)
+    print(butthead)
+    print(my_sweet_home)
+
+# Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
+# Нужен класс Дом, в нем должн быть холодильник с едой и тумбочка с деньгами
+# Еда пусть хранится в холодильнике в доме, а деньги - в тумбочке.
